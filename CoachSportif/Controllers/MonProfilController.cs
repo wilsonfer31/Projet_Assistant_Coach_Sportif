6beacf39dec0d5cf8a08d907b359cc2a0bae1a35
tahmi matti <tahmi.matti01@gmail.com>
2021-11-03 09:41:48 +0100

    aaa

---
+++ CoachSportif/Controllers/MonProfilController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CoachSportif.Controllers
+{
+    public class MonProfilController : Controller
+    {
+        // GET: MonProfil
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ CoachSportif/Controllers/UtilisateursController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using CoachSportif.Models;
+using Coaching_Models;
+
+namespace CoachSportif.Controllers
+{
+    public class UtilisateursController : Controller
+    {
+        private MyContext db = new MyContext();
+
+        // GET: Utilisateurs
+        public ActionResult Index()
+        {
+            return View(db.Utilisateurs.ToList());
+        }
+
+        // GET: Utilisateurs/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Utilisateur utilisateur = db.Utilisateurs.Find(id);
+            if (utilisateur == null)
+            {
+                return HttpNotFound();
+            }
+            return View(utilisateur);
+        }
+
+        // GET: Utilisateurs/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Utilisateurs/Create
+        // Pour vous protéger des attaques par survalidation, activez les propriétés spécifiques auxquelles vous souhaitez vous lier. Pour 
+        // plus de détails, consultez https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Pseudo,MotDePasse,Prenom,Tel,Mail,Adresse,Nom")] Utilisateur utilisateur)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Utilisateurs.Add(utilisateur);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(utilisateur);
+        }
+
+        // GET: Utilisateurs/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Utilisateur utilisateur = db.Utilisateurs.Find(id);
+            if (utilisateur == null)
+            {
+                return HttpNotFound();
+            }
+            return View(utilisateur);
+        }
+
+        // POST: Utilisateurs/Edit/5
+        // Pour vous protéger des attaques par survalidation, activez les propriétés spécifiques auxquelles vous souhaitez vous lier. Pour 
+        // plus de détails, consultez https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Pseudo,MotDePasse,Prenom,Tel,Mail,Adresse,Nom")] Utilisateur utilisateur)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(utilisateur).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(utilisateur);
+        }
+
+        // GET: Utilisateurs/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Utilisateur utilisateur = db.Utilisateurs.Find(id);
+            if (utilisateur == null)
+            {
+                return HttpNotFound();
+            }
+            return View(utilisateur);
+        }
+
+        // POST: Utilisateurs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Utilisateur utilisateur = db.Utilisateurs.Find(id);
+            db.Utilisateurs.Remove(utilisateur);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
