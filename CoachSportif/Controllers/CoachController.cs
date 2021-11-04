997c55be3d57fbba69557a183c97981c16f69265
tahmi matti <tahmi.matti01@gmail.com>
2021-11-04 17:02:43 +0100

    Les Coaches

--- CoachSportif/Controllers/CoachController.cs
+++ CoachSportif/Controllers/CoachController.cs
@@ -1,13 +1,16 @@
-﻿using System.Web.Mvc;
+﻿using CoachSportif.Models;
+using System.Web.Mvc;
+using System.Data.Entity;
 
 namespace CoachSportif.Controllers
 {
     public class CoachController : Controller
     {
+        MyContext db = new MyContext();
         // GET: Coach
         public ActionResult Index()
         {
-            return View();
+            return View(db.Coaches.Include(c=>c.Utilisateur));
         }
     }
 }
